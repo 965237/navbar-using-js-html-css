d2f18b7cbb5ba8091381c66d9243d4a7f33059ff
Ayush Varma <99134940+965237@users.noreply.github.com>
2022-10-17 17:07:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function show() {
+    ele = document.getElementById('navbar').classList.toggle('my');
+    console.log(ele);
+}
+
+
+function show_nav(){
+    lel = document.getElementById('navbar1').classList.toggle('show_menu');
+    console.log(lel);
+
+}
